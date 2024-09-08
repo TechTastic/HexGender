a9d1a8853c86a509d54dff507269f49efbc744ab
TechTastic <74630543+TechTastic@users.noreply.github.com>
2024-09-07 23:52:56 -0500

    Gender Transition spells only target caster and have particles

--- common/src/main/kotlin/io/github/techtastic/hexgender/casting/spells/OpGenderTransition.kt
+++ common/src/main/kotlin/io/github/techtastic/hexgender/casting/spells/OpGenderTransition.kt
@@ -1,23 +1,25 @@
 package io.github.techtastic.hexgender.casting.spells
 
+import at.petrak.hexcasting.api.casting.ParticleSpray
 import at.petrak.hexcasting.api.casting.RenderedSpell
 import at.petrak.hexcasting.api.casting.castables.SpellAction
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
-import at.petrak.hexcasting.api.casting.getPlayer
 import at.petrak.hexcasting.api.casting.iota.Iota
-import at.petrak.hexcasting.api.casting.mishaps.MishapOthersName
-import at.petrak.hexcasting.api.misc.MediaConstants
+import at.petrak.hexcasting.api.casting.mishaps.MishapImmuneEntity
 import com.wildfire.main.GenderPlayer
 import com.wildfire.main.WildfireGender
+import net.minecraft.server.level.ServerPlayer
+import net.minecraft.world.entity.Entity
 
 class OpGenderTransition(val gender: GenderPlayer.Gender): SpellAction {
     override val argc: Int
-        get() = 1
+        get() = 0
 
     override fun execute(args: List<Iota>, env: CastingEnvironment): SpellAction.Result {
-        val player = args.getPlayer(0, argc)
+        val player = env.castingEntity as? ServerPlayer ?: throw MishapImmuneEntity(env.castingEntity as Entity)
         val genderPlayer = WildfireGender.getOrAddPlayerById(player.uuid)
-        return SpellAction.Result(Spell(genderPlayer, gender), MediaConstants.DUST_UNIT, listOf())
+
+        return SpellAction.Result(Spell(genderPlayer, gender), 0L, listOf(ParticleSpray.cloud(player.position().add(0.0, player.eyeHeight / 2.0, 0.0), 1.0)))
     }
 
     private class Spell(val player: GenderPlayer, val gender: GenderPlayer.Gender): RenderedSpell {
